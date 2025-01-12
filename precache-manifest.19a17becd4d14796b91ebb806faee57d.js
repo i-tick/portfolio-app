05bf233b4137ffb006d24372904642aa2aa1959d
Aitik Dandapat <aitikdandapat@Aitiks-MacBook-Air.local>
2025-01-12 14:59:59 -0500

    Updates

---
+++ precache-manifest.19a17becd4d14796b91ebb806faee57d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c12c110969fc714034d7f620f719b8e",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "df752329c3192cbefb71",
+    "url": "/portfolio-app/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "d14e3e541095d0f230c6",
+    "url": "/portfolio-app/static/css/main.5c316139.chunk.css"
+  },
+  {
+    "revision": "df752329c3192cbefb71",
+    "url": "/portfolio-app/static/js/2.3dda4730.chunk.js"
+  },
+  {
+    "revision": "6651528616fcfacfd07f",
+    "url": "/portfolio-app/static/js/3.97a4b452.chunk.js"
+  },
+  {
+    "revision": "d14e3e541095d0f230c6",
+    "url": "/portfolio-app/static/js/main.35bb3747.chunk.js"
+  },
+  {
+    "revision": "7e7ab516ea30e3393d37",
+    "url": "/portfolio-app/static/js/runtime~main.decfa245.js"
+  },
+  {
+    "revision": "ce8fdc1e4bdc2d1c32d3c1c3c3eddff3",
+    "url": "/portfolio-app/static/media/Aitik Resume.ce8fdc1e.pdf"
+  },
+  {
+    "revision": "53a64fba88bcee4cbbd3466ede4b3fd4",
+    "url": "/portfolio-app/static/media/Avtar.53a64fba.jpeg"
+  },
+  {
+    "revision": "6e07f3389d04573c1ad1ce02829c255c",
+    "url": "/portfolio-app/static/media/aitik.6e07f338.jpg"
+  },
+  {
+    "revision": "e71cd20d2b1833462340bc19fbc84d02",
+    "url": "/portfolio-app/static/media/background-image.e71cd20d.jpg"
+  }
+]);
